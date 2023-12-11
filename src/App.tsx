bf789f3690ee5f806fd92ca6d29b5fee80f397bc
nabilahzaid <nabilah.zaid@gmail.com>
2023-12-11 11:52:02 +0800

    update analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Main from './layouts/Main';
-import { Profile } from './components/Profile';
 import { Route, Router, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Interest } from './pages/Interest';
@@ -19,13 +16,6 @@ function App() {
         <Route path="/projects" element={<Project />} />
         <Route path="/interest" element={<Interest />} />
       </Routes>
-      {/* <Main /> */}
-    
-    {/* <Profile nama='Nelle was here' />
-    <Profile nama='Belle was here' />
-    <Profile className='text-3xl font-bold underline text-primary hover:bg-secondary hover:text-white' 
-      nama='Belle was here'/>
-      <Profile nama='Belle 2 was here'/> */}
     </div>
   );
 }
